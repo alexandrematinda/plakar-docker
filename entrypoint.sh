b4f35c1d4115700e2ac286ad52618fdb6f923481
Alexandre Matinda <orangees.72.pagayeur@icloud.com>
2026-05-01 19:01:52 +0200

    fix: check PLAKAR_PASSPHRASE is set before initializing repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 # Initialize kloset if it doesn't exist
 if [ ! -f "/home/plakar/.plakar/CONFIG" ]; then
   echo "Initializing plakar repository..."
+  if [ -z "$PLAKAR_PASSPHRASE" ]; then
+    echo "ERROR: PLAKAR_PASSPHRASE not set. Cannot initialize repository."
+    exit 1
+  fi
   plakar create
 fi
 
